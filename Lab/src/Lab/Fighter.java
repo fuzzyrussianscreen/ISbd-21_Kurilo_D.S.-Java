67b2bb6f70ed601aea00cfe30748599228e378d8
OkCur64 OkCur64 <ok.curilo2010@yandex.ru>
2018-11-30 21:58:04 +0400

    Lab1

---
+++ Lab/src/Lab/Fighter.java
@@ -0,0 +1,167 @@
+package Lab;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Panel;
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+
+public class Fighter {
+
+	private int _startPosX;
+	/// <summary>
+	/// Правая кооридната отрисовки
+	/// </summary>
+	private int _startPosY;
+	/// <summary>
+	/// Ширина окна отрисовки
+	/// </summary>
+	private int _pictureWidth;
+	//Высота окна отрисовки
+	private int _pictureHeight;
+	/// <summary>
+	/// Ширина отрисовки 
+	/// </summary>
+	private final int carWidth = 100;
+	/// <summary>
+	/// Ширина отрисовки 
+	/// </summary>
+	private final int carHeight = 60;
+
+	private int MaxSpeed;
+	void getMaxSpeed(int MaxSpeed) {this.MaxSpeed=MaxSpeed;}
+	int setMaxSpeed() {return this.MaxSpeed;}
+
+	private int Weight;
+	void getWeight(int Weight) {this.Weight=Weight;}
+	int setWeight() {return this.Weight;}
+
+	private Color MainColor;
+	void getMainColor(Color MainColor) {this.MainColor=MainColor;}
+	Color setMainColor() {return this.MainColor;}
+
+	private Color DopColor;
+	void getDopColor(Color DopColor) {this.DopColor=DopColor;}
+	Color setDopColorr() {return this.DopColor;}
+
+	private boolean FrontSpoiler;
+	void getFrontSpoiler(boolean FrontSpoiler) {this.FrontSpoiler=FrontSpoiler;}
+	boolean setFrontSpoilert() {return this.FrontSpoiler;}
+
+	private boolean MiddleSpoiler;
+	void getMiddleSpoiler(boolean MiddleSpoiler) {this.MiddleSpoiler=MiddleSpoiler;}
+	boolean setMiddleSpoilert() {return this.MiddleSpoiler;}
+
+	private boolean BackSpoiler;
+	void getBackSpoiler(boolean BackSpoiler) {this.BackSpoiler=BackSpoiler;}
+	boolean setBackSpoiler() {return this.BackSpoiler;}
+
+	public Fighter(int maxSpeed, int weight, Color mainColor, Color dopColor, boolean
+			frontSpoiler, boolean middleSpoiler, boolean backSpoiler)
+	{
+		MaxSpeed = maxSpeed;
+		Weight = weight;
+		MainColor = mainColor;
+		DopColor = dopColor;
+		MiddleSpoiler = middleSpoiler;
+		FrontSpoiler = frontSpoiler;
+		BackSpoiler = backSpoiler;
+	}
+	
+	public void SetPosition(int x, int y, int width, int height)
+	{
+		_startPosX = x;
+		_startPosY = y;
+		_pictureWidth = width;
+		_pictureHeight = height;
+	}
+	
+	public void MoveTransport(Direction direction)
+	{
+		float step = MaxSpeed * 100 / Weight;
+		switch (direction)
+		{
+		// вправо
+		case Right:
+			if (_startPosX + step < _pictureWidth - carWidth)
+			{
+				_startPosX += step;
+			}
+			break;
+			//влево
+		case Left:
+			if (_startPosX - step > 0)
+			{
+				_startPosX -= step;
+			}
+			break;
+			//вверх
+		case Up:
+			if (_startPosY - step > 0)
+			{
+				_startPosY -= step;
+			}
+			break;
+			//вниз
+		case Down:
+			if (_startPosY + step < _pictureHeight - carHeight)
+			{
+				_startPosY += step;
+			}
+			break;
+		}
+	}
+	/// <summary>
+	/// Отрисовка
+	/// </summary>
+	/// <param name="g"></param>
+	public void DrawFighter(Graphics gr)
+	{
+		
+		Graphics2D g = (Graphics2D)gr;
+		Stroke oldStroke = g.getStroke();
+		float[] shtrich = {14, 5};
+		BasicStroke bs = new BasicStroke(8, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND, 3, shtrich, 0);
+		g.setStroke(bs);
+
+		if (BackSpoiler)
+		{
+			g.setColor(MainColor);
+			g.drawLine( _startPosX + 31, _startPosY - 24, _startPosX + 31, _startPosY + 24);
+			g.setColor(DopColor);
+			g.drawLine( _startPosX + 4, _startPosY - 5, _startPosX, _startPosY - 20);
+			g.drawLine( _startPosX + 14, _startPosY - 5, _startPosX, _startPosY - 20);
+			g.drawLine( _startPosX + 4, _startPosY + 5, _startPosX, _startPosY + 20);
+			g.drawLine( _startPosX + 14, _startPosY + 5, _startPosX, _startPosY + 20);
+		}
+		if (MiddleSpoiler)
+		{
+			g.setColor(DopColor);
+			g.drawLine( _startPosX + 24, _startPosY - 5, _startPosX + 22, _startPosY - 50);
+			g.drawLine( _startPosX + 40, _startPosY - 5, _startPosX + 22, _startPosY - 50);
+			g.drawLine( _startPosX + 24, _startPosY + 5, _startPosX + 22, _startPosY + 50);
+			g.drawLine( _startPosX + 40, _startPosY + 5, _startPosX + 22, _startPosY + 50);
+
+			g.drawLine( _startPosX + 46, _startPosY , _startPosX + 55, _startPosY + 10);
+			g.drawLine( _startPosX + 46, _startPosY , _startPosX + 55, _startPosY - 10);
+		}
+		g.setColor(Color.BLACK);
+		g.fillRect( _startPosX, _startPosY - 6, 80, 12);
+		g.setColor(MainColor);
+		g.drawLine( _startPosX + 79, _startPosY - 3, _startPosX + 100, _startPosY);
+		g.drawLine( _startPosX + 79, _startPosY + 3, _startPosX + 100, _startPosY);
+		if (FrontSpoiler)
+		{
+			g.setColor(DopColor);
+			g.drawLine( _startPosX + 100, _startPosY, _startPosX + 110, _startPosY);
+		}
+	}
+}
---
+++ Lab/src/Lab/Main.java
@@ -0,0 +1,125 @@
+package Lab;
+import java.awt.Button;
+
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+
+
+public class Main {
+
+	private JFrame frame;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public Main() {
+
+		initialize();
+	}
+	
+	Fighter fighter;
+	private JPanel panel;
+	private JButton buttonLeft;
+
+	
+	private void initialize() {
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 958, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+
+		Button button = new Button("Create");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Random rnd = new Random();
+				fighter = new Fighter(rnd.nextInt(200)+500, rnd.nextInt(1000)+1000, Color.BLACK, Color.GRAY, true, true, true);
+				panel = new PanelHungar(fighter);
+				panel.setBounds(20, 20, 700, 401);
+				frame.getContentPane().add(panel);
+				fighter.SetPosition(rnd.nextInt(100)+50, rnd.nextInt(100)+50, panel.getWidth(), panel.getHeight());
+				panel.repaint();
+				
+			}
+		});
+		button.setBounds(851, 10, 79, 24);
+		frame.getContentPane().add(button);
+		
+		buttonLeft = new JButton("");
+		buttonLeft.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fighter.MoveTransport(Direction.Left);
+				panel.repaint();
+			}
+		});
+		buttonLeft.setVerticalAlignment(SwingConstants.BOTTOM);
+		buttonLeft.setIconTextGap(2);
+		
+		buttonLeft.setIcon(new ImageIcon("D:\\ФИСТ ИСЭбд\\Тех. программирование\\ISbd-21_Kurilo_D.S-java\\Lab\\arrow1.png"));
+		//btnNewButton.setIco
+		buttonLeft.setBounds(756, 490, 50, 50);
+		frame.getContentPane().add(buttonLeft);
+		
+		JButton buttonDown = new JButton("");
+		buttonDown.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fighter.MoveTransport(Direction.Down);
+				panel.repaint();
+			}
+		});
+		buttonDown.setIcon(new ImageIcon("D:\\ФИСТ ИСЭбд\\Тех. программирование\\ISbd-21_Kurilo_D.S-java\\Lab\\arrow4.png"));
+		buttonDown.setVerticalAlignment(SwingConstants.BOTTOM);
+		buttonDown.setIconTextGap(2);
+		buttonDown.setBounds(818, 490, 50, 50);
+		frame.getContentPane().add(buttonDown);
+		
+		JButton buttonUp = new JButton("");
+		buttonUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fighter.MoveTransport(Direction.Up);
+				panel.repaint();
+			}
+		});
+		buttonUp.setIcon(new ImageIcon("D:\\ФИСТ ИСЭбд\\Тех. программирование\\ISbd-21_Kurilo_D.S-java\\Lab\\arrow3.png"));
+		buttonUp.setVerticalAlignment(SwingConstants.BOTTOM);
+		buttonUp.setIconTextGap(2);
+		buttonUp.setBounds(818, 427, 50, 50);
+		frame.getContentPane().add(buttonUp);
+		
+		JButton buttonRight = new JButton("");
+		buttonRight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fighter.MoveTransport(Direction.Right);
+				panel.repaint();
+			}
+		});
+		buttonRight.setIcon(new ImageIcon("D:\\ФИСТ ИСЭбд\\Тех. программирование\\ISbd-21_Kurilo_D.S-java\\Lab\\arrow2.png"));
+		buttonRight.setVerticalAlignment(SwingConstants.BOTTOM);
+		buttonRight.setIconTextGap(2);
+		buttonRight.setBounds(880, 490, 50, 50);
+		frame.getContentPane().add(buttonRight);
+		
+	}
+}
